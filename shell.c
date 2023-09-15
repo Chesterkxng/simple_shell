14733211fb05d78c7b6627643ed510850e61410a
Dodzi Keglo <stephanekeglo@gmail.com>
2023-09-15 12:23:40 +0000

    Display the prompt only in interactive mode

--- shell.c
+++ shell.c
@@ -1,20 +1,28 @@
 #include "main.h"
 /**
  * main - unix command interpreter
+ * @argc: number of arguments
+ * @argv: list of shell arguments
  * Description: unix command interpreter
  * Return: 0 always success
  */
-int main(void)
+int main(int argc, char **argv)
 {
 	char *line;
 	char **args;
+	(void) argc;
 
 	do {
-		prompt();
+		/* display the prompt only in interactive mode */
+		if (isatty(0))
+			prompt();
+
 		line = get_input_line();
 		args = build_args(line);
-		exec_cmd(args);
+		exec_cmd(args, argv);
 		free(line);
+
 	} while (1);
+
 	return (0);
 }
